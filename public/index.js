c502ab67991be2a795f2a8b62f17eca76b988fca
yuganksinghal <s.yugank@gmail.com>
2024-03-09 06:11:54 +0000

    add basic collaboration. one canvas for the whole world

--- public/index.js
+++ public/index.js
@@ -4,6 +4,30 @@ const rows = 8;
 const columns = 32;
 var matrixData = new Array(rows*columns);
 const total = rows * columns;
+const lightSocket = new WebSocket(`ws://${window.location.host}/matrix`);
+
+lightSocket.onmessage = (event) => {
+    lsData = JSON.parse(event.data);
+    if(lsData.length === 0) {
+        setupBoard();
+        return
+    }
+    const LEDMatrix = document.querySelector('#LEDMatrix');
+    let children = []
+    for (let i = 0; i < lsData.length; i++) {
+        let LED = document.createElement('div');
+        LED.classList.add("led");
+        // LED.innerHTML = `${i} | ${findIndex(i)}`; // debug line
+        let index = findIndex(i);
+        LED.dataset.matrixIndex = index;
+        if (lsData[index] && lsData[index] !== "#000") LED.style.backgroundColor = lsData[index];
+        LED.addEventListener('mousedown', (event) => paintPixel(event));
+        LED.addEventListener('mouseover', (event) => dragPixel(event));
+        children.push(LED);
+    }
+    matrixData = lsData;
+    LEDMatrix.replaceChildren(...children);
+}
 
 function setupBoard() {
     colorPicker = document.querySelector('#colorPicker');
@@ -42,7 +66,8 @@ function paintPixel(e) {
     const pixel = e.target;
     pixel.style.backgroundColor = eraseState ? '' : paintbrush;
     matrixData[pixel.dataset.matrixIndex] = eraseState ? 'off' : paintbrush;
-    sendData();
+    //sendData();
+    lightSocket.send(JSON.stringify({'LEDMatrix': matrixData}));
 }
 
 async function sendData(){
